653a0af3e98ebc22e794ee4c14143326107d0fff
Tran Duy Hung <duyhung6604@gmail.com>
2025-02-20 22:57:20 +0700

    34

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,12 @@
+const TodoData = (props) => {
+    console.log("check props:" , props)
+    return (
+        <div className='todo-data'>
+            <div>My name is {props.name}</div>
+          <div>Learing React</div>
+          <div>Watchinh Youtube</div>
+      </div>
+    )
+}
+
+export default TodoData;
